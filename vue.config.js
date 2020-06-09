a006c7ed14e0145930bce792537edf36ad2e725a
姜志远 <sdwdjzhy@qq.com>
2020-06-09 09:53:03 +0800

    更新package 更新vue的配置选项

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const resolve = dir => {
     return path.join(__dirname, dir);
@@ -8,57 +7,60 @@ const isProduction = process.env.NODE_ENV === "production";
 
 const BASE_URL = process.env.NODE_ENV === "development" ? "/" : "/";
 
-const PAGE_ENTRIES = [{
+const PAGE_ENTRIES = [
+    {
         entry: "home",
         title: "首页",
     },
     {
         entry: "admin",
         title: "管理",
-    }
+    },
 ];
 
+var pages = {};
+PAGE_ENTRIES.forEach(i => {
+    pages[i.entry] = {
+        entry: `./src/entries/${i.entry}.js`,
+        title: i.title,
+        chunks: ["chunk-vendors", "chunk-common", i.entry],
+    };
+});
+
 module.exports = {
-    publicPath: "/",
-    outputDir: "/dist/",
+    publicPath: BASE_URL,
     // 如果你不需要使用eslint，把lintOnSave设为false即可
     lintOnSave: true,
-
+    pages,
     //filenameHashing: isProduction ? false : true,
     chainWebpack: config => {
         config.resolve.alias
             .set("@", resolve("src")) // key,value自行定义，比如.set('@@', resolve('src/components'))
             .set("_c", resolve("src/components"));
 
-        config.entryPoints.delete("app");
-        config.plugins.delete("html");
-        PAGE_ENTRIES.forEach(v => {
-            config.entry(v.entry).add(`./src/entries/${v.entry}.js`);
-            config.plugin(`html-${v.entry}`).use(HtmlWebpackPlugin, [{
-                templateParameters: {
-                    BASE_URL: "/",
-                },
-                title: v.title,
-                // template: `./src/entries/${v}/index.html`,
-                // entry: `./src/entries/${v}/main.js`,
-                template: "./public/index.html",
-                entry: `./src/entries/${v.entry}.js`,
-                filename: `${v.entry}.html`,
-                inject: true,
-                excludeChunks: PAGE_ENTRIES.filter(item => item !== v),
-            }]).before("preload");
-        });
+        // 使用 vue inspect --plugins > output.js 命令，查看所有的plugin，
+        // 如果不要生成 某个html，例如和 home 的
+        // config.plugins.delete("html-home");
     },
 
     configureWebpack: config => {
-        // if (isProduction) {
-        //     config.externals = {
-        //         vue: "Vue",
-        //         lodash: "_",
-        //         axios: "axios",
-        //         "element-ui": "ELEMENT"
-        //     };
-        // }
+        if (isProduction) {
+            config.externals = {
+                vue: "Vue",
+                "vue-i18n": "VueI18n",
+                "vue-router": "VueRouter",
+                "awe-dnd": "VueDragging",
+                axios: "axios",
+                clipboard: "ClipboardJS",
+                iview: "iview",
+                "js-cookie": "Cookies",
+                jquery: "jQuery",
+                lodash: "lodash",
+                moment: "moment",
+                sortablejs: "Sortable",
+                "view-design": "iview",
+            };
+        }
     },
 
     // 打包时不生成.map文件
@@ -73,10 +75,10 @@ module.exports = {
                 target: "http://localhost:5000/", // 接口域名
                 ws: true,
                 pathRewrite: {
-                    "^/apis": "/"
+                    "^/apis": "/",
                 },
-                changeOrigin: true // 是否跨域
-            }
+                changeOrigin: true, // 是否跨域
+            },
         },
         overlay: {
             warnings: false,
